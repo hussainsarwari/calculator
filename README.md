57d7baf67296d1c0ba79683741a08ff8e8d8423b
hussainsarwari <mohammadhussainsarwari5@gmail.com>
2023-08-16 21:48:52 +0430

    it is first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+
+<div align='center'>
+
+<h1>javascript calculatore</h1>
+<p>this calculatore created by javascript and html css</p>
+
+<h4> <span> · </span> <a href="https://github.com/hussainsarwari/html-css-js-calculator.github.io/blob/master/README.md"> Documentation </a> <span> · </span> <a href="https://github.com/hussainsarwari/html-css-js-calculator.github.io/issues"> Report Bug </a> <span> · </span> <a href="https://github.com/hussainsarwari/html-css-js-calculator.github.io/issues"> Request Feature </a> </h4>
+
+
+</div>
+
+# :notebook_with_decorative_cover: Table of Contents
+
+- [About the Project](#star2-about-the-project)
+- [Contact](#handshake-contact)
+
+
+## :star2: About the Project
+
+## :wave: Contributing
+
+<a href="html-css-js-calculator.github.io/graphs/contributors"> <img src="https://contrib.rocks/image?repo=Louis3797/awesome-readme-template" /> </a>
+
+Contributions are always welcome!
+
+see `contributing.md` for ways to get started
+
+### :scroll: Code of Conduct
+
+Please read the [Code of Conduct](html-css-js-calculator.github.io/blob/master/CODE_OF_CONDUCT.md)
+
+## :handshake: Contact
+
+Mohammad Hussain Sarwari - - sarwarihussain7@gmail.com
+
+Project Link: [html-css-js-calculator.github.io](html-css-js-calculator.github.io)
---
+++ js/learn javascript.js
@@ -0,0 +1,101 @@
+let clear = document.querySelector("#clear");
+let resulte = document.querySelector("#resulte");
+let btns = document.querySelectorAll("button");
+let Alert = document.querySelector(".alert");
+let option_box = document.querySelector("div.option");
+let count = 0;
+let arr = [];
+let close_option_btn = document.querySelector("h4");
+close_option_btn.addEventListener("click", () => {
+  option_box.classList.remove("show_option");
+});
+
+for (const btn of btns) {
+  btn.addEventListener("click", () => {
+    // debugger;
+    if (btn.classList.value == "number" || btn.classList.value == "operation") {
+      arr.push(btn.textContent);
+      control(++count, btn);
+    } else if (btn.classList.value == "clear") {
+      resulte.textContent = "";
+      count = 0;
+    } else if (
+      btn.classList.value == "option" ||
+      btn.classList.value == "option1"
+    ) {
+      option_box.classList.add("show_option");
+    } else {
+      resulte.textContent = eval(resulte.textContent);
+      count = 0;
+    }
+  });
+}
+
+// control the inputs
+
+function control(a, button) {
+  if (a == 1) {
+    if (button.textContent == 0) {
+      Alert.classList.add("show_alert");
+      setTimeout(() => {
+        Alert.classList.remove("show_alert");
+      }, 900);
+      count = 0;
+    } else if (button.classList.value == "operation") {
+      Alert.classList.add("show_alert");
+      setTimeout(() => {
+        Alert.classList.remove("show_alert");
+      }, 800);
+      count = 0;
+    } else {
+      resulte.innerText += button.innerText;
+    }
+  } else {
+    let flag = false;
+    if (button.classList.value == "operation") {
+      if (
+        arr[a - 2] == "-" ||
+        arr[a - 2] == "+" ||
+        arr[a - 2] == "*" ||
+        arr[a - 2] == "/"
+      ) {
+        Alert.classList.add("show_alert");
+        setTimeout(() => {
+          Alert.classList.remove("show_alert");
+        }, 800);
+        count = 0;
+        flag = true;
+      }
+      if (flag) {
+        resulte.innerText = "";
+      } else resulte.innerText += button.innerText;
+    } else resulte.innerText += button.innerText;
+  }
+}
+
+function show_history() {
+  document.querySelector(".history").classList.add("show_history");
+}
+function close_history() {
+  document.querySelector(".history").classList.remove("show_history");
+}
+let f = true; //advance calculator
+function show_advCalculator() {
+  debugger;
+
+  document.querySelector("div.btns").style.display = "none";
+  document.querySelector(".adv_cal_btns").style.display = "grid";
+  option_box.classList.remove("show_option");
+  if (document.querySelector("#adv_calcu").textContent == "sample calculator") {
+    document.querySelector(".adv_cal_btns").style.display = "none";
+    document.querySelector(".btns").style.display = "grid";
+  }
+
+  if (f) {
+    document.querySelector("#adv_calcu").textContent = "sample calculator";
+    f = false;
+  } else {
+    document.querySelector("#adv_calcu").textContent = "advance calculator";
+    f = true;
+  }
+}
